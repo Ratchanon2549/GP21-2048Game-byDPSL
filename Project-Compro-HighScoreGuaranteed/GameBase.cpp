38e030c92acd74306e87e4c0c8ae61b1a9b28018
David19 <101934299+DavidPPD@users.noreply.github.com>
2025-05-28 17:06:00 +0700

    Delete Project-Compro-HighScoreGuaranteed directory

--- Project-Compro-HighScoreGuaranteed/GameBase.cpp
+++
@@ -1,5 +0,0 @@
-#include "GameBase.h"
-
-GameBase::GameBase() {
-    
-}
--- Project-Compro-HighScoreGuaranteed/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "ScoreTrackingGame.h"
-#include "GameManager.h"
-
-int main() {
-    GameManager manager(new ScoreTrackingGame());
-    manager.run();
-    return 0;
-}
